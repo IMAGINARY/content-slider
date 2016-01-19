44aa369fdbf05dd6f563347674b086941fc4b445
Christian Stussak <stussak@mfo.de>
2016-01-19 23:39:04 +0100

    reload page at midnight

--- content-slider.php
+++ content-slider.php
@@ -61,6 +61,24 @@
     	<link rel="stylesheet" href="css/style.css">
         <script src="js/jssor.slider.min.js"></script>
         <script>
+        function refreshAt(hours, minutes, seconds) {
+            var now = new Date();
+            var then = new Date();
+
+            if(now.getHours() > hours ||
+               (now.getHours() == hours && now.getMinutes() > minutes) ||
+                now.getHours() == hours && now.getMinutes() == minutes && now.getSeconds() >= seconds) {
+                then.setDate(now.getDate() + 1);
+            }
+            then.setHours(hours);
+            then.setMinutes(minutes);
+            then.setSeconds(seconds);
+
+            var timeout = (then.getTime() - now.getTime());
+            setTimeout(function() { window.location.reload(true); }, timeout);
+        }
+        refreshAt( 0, 0, 0 );
+
         jssor_top_slider_starter = function (containerId) {
             var options = {
                 $AutoPlay: true,

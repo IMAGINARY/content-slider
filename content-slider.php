37df267519a9c80acbe99edfd3942bf739dd4adc
Christian Stussak <stussak@mfo.de>
2016-01-27 10:47:27 +0100

    prevent pinch-to-zoom

--- content-slider.php
+++ content-slider.php
@@ -64,6 +64,8 @@
 <html>
     <head>
     	<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
     	<title>Content Slider</title>
     	<link rel="stylesheet" href="css/style.css">
         <script src="js/jssor.slider.min.js"></script>

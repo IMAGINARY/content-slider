d03a2bd654acba74fd691fa6abd3717979076e11
Christian Stussak <stussak@mfo.de>
2019-07-09 14:25:10 +0200

    Add some default implementations to the Application class

--- js/application.js
+++ js/application.js
@@ -3,18 +3,25 @@ class Application {
     }
 
     get ready() {
+        return Promise.resolve();
     }
 
     get domElement() {
+        if( typeof this._domElement === 'undefined')
+            this._domElement = document.createElement('div');
+        return this._domElement;
     }
 
     get name() {
+        return 'Application'
     }
 
     get description() {
+        return 'Application description.'
     }
 
     get credits() {
+        return 'Application credits'
     }
 
     pause() {

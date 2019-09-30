47ccf1832db2c44075d852e1b4b6fafe1d026059
Christian Stussak <stussak@mfo.de>
2019-09-30 10:59:47 +0200

    Refactor LibLoader to utilize Fetcher

--- js/LibLoader.js
+++ js/LibLoader.js
@@ -1,5 +1,6 @@
-const countdownJsPromise = fetch(new URL('../vendor/countdown.js/2.6.0/countdown.min.js', import.meta.url).href)
-    .then(responde => responde.text())
+import {Fetcher} from './Utils.js';
+
+const countdownJsPromise = Fetcher.fetchText(new URL('../vendor/countdown.js/2.6.0/countdown.min.js', import.meta.url).href)
     .then(scriptText => eval(scriptText + "; countdown;"));
 
 async function countdownjs() {

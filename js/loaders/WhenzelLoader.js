7f86d3eb4cb333f43ff172f9966e628bc633ffbe
Christian Stussak <stussak@mfo.de>
2019-10-02 11:18:52 +0200

    Add loader for general Whenzel filterable lists of objects
    Each object in the list must contain a 'when' property as well as a data
    property. The data property can hold arbitrary user-defined values which
    are not validated further.

---
+++ js/loaders/WhenzelLoader.js
@@ -0,0 +1,19 @@
+import {AjvUtils, Fetcher} from '../Utils.js';
+import '../../vendor/whenzel/1.0.2/whenzel.js';
+
+let validateFuncPromise = AjvUtils.getValidateFunc(new URL("../../schema/whenzelSchema.yaml", import.meta.url));
+
+async function load(url, now) {
+    try {
+        const yaml = await Fetcher.fetchYaml(url);
+        const entries = {};
+        entries.all = await AjvUtils.validateAndThrowOnError(yaml, await validateFuncPromise);
+        entries.filtered = entries.all.filter(wentry => Whenzel.test(entry.when, now));
+        return entries;
+    } catch (err) {
+        err.message = `Unable to process ${url.href}: ${err.message}`;
+        return {all: [], filtered: [], error: err};
+    }
+}
+
+export {load};
